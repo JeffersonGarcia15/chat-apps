486de462448b5357e551381bafaeaf0b46c7db98
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2024-02-02 19:17:42 -0600

    short-polling/server draft

---
+++ short-polling/server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import logger from "morgan";
+
+const port = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(logger("dev"));
+
+const messages = [];
+
+app.get("/", (req, res) => {
+    res.sendFile(process.cwd() + "/client/index.html");
+});
+
+app.get("/messages", (req, res) => {
+    res.json(messages);
+});
+
+app.post("/messages", (req, res) => {
+    const { message } = req.body;
+    messages.push({
+        id: messages.length + 1,
+        message,
+    });
+    res.status(201).json(message);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
